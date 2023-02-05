5a8e7ee9195dbdebc689d719d68dee5d764efd08
Alexey Militskiy <alexei.militski@gmail.com>
2023-02-05 13:34:00 +0400

    Refactoring: missed Repository annotation in stats server

--- stats-service/stats-server/src/main/java/ru/practicum/ewm/stats/repository/StatsMongoRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/ewm/stats/repository/StatsMongoRepository.java
@@ -1,8 +1,10 @@
 package ru.practicum.ewm.stats.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.stats.model.Hit;
 
+@Repository
 public interface StatsMongoRepository extends ReactiveMongoRepository<Hit, String> {
 
 }
